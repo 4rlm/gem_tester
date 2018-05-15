e227cc94f9c3fc245b3940166dcef3d73ba4f02f
Adam Booth <4rlm@protonmail.ch>
2018-05-15 15:19:10 -0500

    testing crm

--- lib/crm_formatter.rb
+++
@@ -1,9 +0,0 @@
-require 'crm_formatter/address'
-require 'crm_formatter/web'
-require 'crm_formatter/phone'
-require 'crm_formatter/helpers'
-require 'crm_formatter/version'
-
-module CRMFormatter
-
-end
--- lib/crm_formatter/version.rb
+++
@@ -1,3 +0,0 @@
-module CRMFormatter
-  VERSION = "1.0.6-rc.1"
-end
